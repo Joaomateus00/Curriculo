fba406619576cd1e1b317ff96205254ee7a4c709
Joaomateus00 <joaoommateus@gmail.com>
2022-09-06 14:48:45 -0300

    Implementação da aba projetos

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function onOver(elemento){
-    elemento.style.textDecoration = "underline"
+function onOver(sobre){
+    let about = document.getElementById('link-sobre');
+    about.innerHTML = 'About';
+}
+function onOverp(proj){
+    let project = document.getElementById('link-proj');
+    project.innerHTML = 'Project';
+}
+function onOutproj(projout){
+    let project = document.getElementById('link-proj')
+    project.innerHTML = 'Projetos';
+}
+function onOutsob(sobreout){
+    let sobre = document.getElementById('link-sobre')
+    sobre.innerHTML = 'Sobre';
+}
+function onOverprg(tradutor){
+    let translate = document.getElementById('paragrafo-prin');
+    translate.innerHTML = "Hello, my name is João Mateus, I'm 22 years old and I'm learning Web Development, so far I don't have any training at a higher education institution but I'm taking the BR Programmer course: Full Stack Web Development. I recently started to get interested in programming and Web Development, rigth away I was fascinated by so many possibilities. I've been through programming logic whit Professor Gustavo Guanabara and even Pyhton througth the same. Now I'm more focused on Web Development, and every day I wake up wanting to modify or create something. Programming is already such part of my life that I can't see myself working with it, I'm looking for the opportunity."
+}
+function skillOn(habil){
+    let skill = document.getElementById('titulo-hab');
+    skill.innerHTML = "Skill";
+}
+function skillOut(habil){
+    let skillout = document.getElementById('titulo-hab');
+    skillout.innerHTML = "Habilidades";
 }
